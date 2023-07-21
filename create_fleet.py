7a44cded10a47f7fcee1fc57652365935fdff03b
Maria Kokoreva <kkmarushka@gmail.com>
2023-07-21 19:52:06 +0200

    added validation for coordinates range input

--- create_fleet.py
+++ create_fleet.py
@@ -10,7 +10,7 @@ def enter_coordinates():  # create any ship coordinates tuple
     medium_ships = []
     small_ships = []
 
-    ships = [2, 3, 5]
+    ships = [2, 3, 5]  # list of ship sizes for verification of input ship_size
     min_allowed_value = 0
     max_allowed_value = 9
     
@@ -21,14 +21,14 @@ def enter_coordinates():  # create any ship coordinates tuple
         try:
             ship_size = int(input("Enter the size of the ship you want to create (5, 3 or 2): "))
         except ValueError:  # if user inserts non integer
-            print("Only numbers are allowed. Insert number 0-9.")
+            print("Only numbers 5, 3 and 2 are allowed. Try again.")
             continue
         else:  # if the user inserts incorrect size number
             if ship_size not in ships:
                 print(f"The {ship_size} size is not allowed. Choose the correct size.")
                 continue
 
-        # validation for the ships limit
+        # validation for the ships amount limit
         if ship_size == 5 and len(big_ship) == 1:
             print("You already have big ship, choose another size.")
             continue 
@@ -49,28 +49,36 @@ def enter_coordinates():  # create any ship coordinates tuple
         while ship_length != ship_size:  # ask user to enter coordinates until the ship has required size
             try:
                 coordinates_column = int(input(f"Input column coordinate for the ship deck {deck} : "))
+                if coordinates_column < min_allowed_value or coordinates_column > max_allowed_value:
+                    print("This position is out of grid. Insert number 0-9.")
+                    continue
                 coordinates_row = int(input(f"Input row coordinate for the ship deck {deck} : "))
-                if (coordinates_column, coordinates_row) not in fleet:
+                if coordinates_row < min_allowed_value or coordinates_row > max_allowed_value:
+                    print("This position is out of grid. Insert number 0-9.")
+                    continue
+                if (coordinates_column, coordinates_row) not in fleet:  # if position is available, add to fleet
                     fleet.append((coordinates_column, coordinates_row))
                     ship_length += 1
                     deck += 1
-                else:
+
+                else:  # handling occupied position
                     print("You already placed ship there. Choose another position.")
                     continue
-            except ValueError:
+            except ValueError:  # handling incorrect input type
                 print("Only numbers are allowed. Try again.")
                 continue
 
-        # conditions for limit of ships
+        # conditions for limit of ships and informing player of the ships position
+
         if ship_size == 5:
-            big_ship.append('X')
-            print("Big ship list: ", big_ship)
+            big_ship.append("X")
+            print("Big ship: ", big_ship)
         elif ship_size == 3:
-            medium_ships.append('X')
-            print("Medium ships list: ", medium_ships)
+            medium_ships.append("X")
+            print("Medium ships: ", medium_ships)
         elif ship_size == 2:
-            small_ships.append('X')
-            print("Small ships list: ", small_ships)
+            small_ships.append("X")
+            print("Small ships: ", small_ships)
 
     return fleet
 
